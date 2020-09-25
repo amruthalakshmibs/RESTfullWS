a6836eb879ff02bbbe0e351d5bb0f1a4fa26d25e
Amrutha <amrithalakshmibs@gmail.com>
2020-09-25 15:26:33 +0530

    First commit

---
+++ restApp/src/main/java/org/examples/showroom/services/BrandsService.java
@@ -0,0 +1,30 @@
+package org.examples.showroom.services;
+
+import java.util.List;
+
+import org.examples.showroom.hibernate.DAO.BrandsDAO;
+import org.examples.showroom.hibernate.entities.BrandEntity;
+
+public class BrandsService {
+	BrandsDAO DAO = new BrandsDAO();
+	public List<BrandEntity> getBrands() {
+		List<BrandEntity> list = DAO.getBrands();
+		return list;
+	}
+
+	public void addBrand(BrandEntity brand) {
+		DAO.addBrand(brand);
+		
+	}
+
+	public void updateBrand(BrandEntity updatedBrand) {
+		DAO.updateBrand(updatedBrand);
+		
+	}
+
+	public void deleteBrand(int brandId) {
+		DAO.deleteBrand(brandId);
+		
+	}
+
+}
